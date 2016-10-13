4fc2fbc618b248242fabe41f7246e03872861a1f
Calvin Wong <man@workmarket.com>
2016-10-13 11:07:23 -0400

    CalvinW: -Updated the horizontal offset calculation as it is not correct on API 23 or above devices.

--- library-addition/src/main/java/io/techery/progresshint/addition/HorizontalProgressHintDelegate.java
+++ library-addition/src/main/java/io/techery/progresshint/addition/HorizontalProgressHintDelegate.java
@@ -32,7 +32,7 @@ public class HorizontalProgressHintDelegate extends ProgressHintDelegate {
   }
 
   private int getHorizontalOffset(int progress) {
-    return getFollowPosition(progress) - mPopupView.getMeasuredWidth() / 2 + mSeekBar.getHeight() / 2;
+    return getFollowPosition(progress) - mPopupView.getMeasuredWidth() / 2 + mSeekBar.getPaddingLeft();
   }
 
   private int getVerticalOffset() {
